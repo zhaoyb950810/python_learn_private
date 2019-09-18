a80acb12cfcfe944f985ee90df2055dd0fba95b1
zhaoyanbing <zhaoyanbing@jiuqi.com.cn>
2019-09-18 22:56:17 +0800

    test

---
+++ day02/varDesc.py
@@ -0,0 +1,43 @@
+"""
+使用变量保存数据病进行算术运算
+
+a = 321
+b = 123
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+print(a ** b)
+"""
+"""
+使用input()函数获取键盘输入
+使用int()进行类型转换
+用占位符格式化输出的字符串
+
+a = int(input('a = '))
+b = int(input('b = '))
+print('%d + %d = %d' % (a, b, a + b))
+print('%d - %d = %d' % (a, b, a - b))
+print('%d * %d = %d' % (a, b, a * b))
+print('%d / %d = %f' % (a, b, a / b))
+print('%d // %d = %d' % (a, b, a // b))
+print('%d %% %d = %d' % (a, b, a % b))
+print('%d ** %d = %d' % (a, b, a ** b)
+"""
+"""
+使用type()检查变量的类型
+a = 100
+b = 12.345
+c = 1 + 5j
+d = 'hello, world'
+e = True
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+
+"""
+
