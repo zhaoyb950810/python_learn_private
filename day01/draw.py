fe3c996b12b4cca418ec05ecd2d52149728fb300
zhaoyanbing <zhaoyanbing@jiuqi.com.cn>
2019-09-18 15:32:49 +0800

    first commit

---
+++ day01/draw.py
@@ -0,0 +1,20 @@
+import turtle
+
+turtle.pensize(1)
+turtle.pencolor('red')
+
+turtle.forward(200)
+turtle.right(30)
+turtle.forward(200)
+turtle.right(90)
+turtle.forward(200)
+turtle.right(90)
+turtle.forward(200)
+turtle.right(90)
+turtle.forward(200)
+turtle.right(180)
+turtle.forward(200)
+turtle.right(120)
+turtle.forward(200)
+
+turtle.mainloop()
